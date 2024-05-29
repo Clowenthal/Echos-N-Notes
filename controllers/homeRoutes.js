880648ac1e1090306db55864a112e751fe770dae
Giovanna Domingues <giovanna.motta.d@outlook.com>
2024-05-28 20:42:04 -0700

    structure code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,95 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+const randomUrlGen = require("random-youtube-music-video");
+
+
+router.get('/', async (req, res) => {
+  try {
+    const youtubeUrl = await randomUrlGen.getRandomMusicVideoUrl();
+    console.log("this is youtubeUrl", youtubeUrl)
+    console.log(youtubeUrl.replace("watch?v=", "embed/"));
+    const newYoutubeUrl = youtubeUrl.replace("watch?v=", "embed/");
+    console.log("This is new youtubeUrl ", newYoutubeUrl)
+    //youtubeUrl = youtubeUrl.replace("watch?v=", "v/");
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+      newYoutubeUrl
+      
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          attributes: ['content', 'date_created'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+    console.log(post);
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
---
+++ public/js/dashboard.js
@@ -0,0 +1,27 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-content').value.trim();
+
+  console.log(name, content);
+
+  if (name && content) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ name, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
